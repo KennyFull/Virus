6dc3533fe5094327c4cfbda31dbb1990118dd8ee
KennyFull <Mr.vazilevz@yandex.ru>
2025-02-26 18:51:32 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿using ConsoleApp1.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                (int, int) YAndE;
+                Console.WriteLine("Введите сколько народа будет в вашем городе");
+                YAndE.Item1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите сколько дней будет длится эпидемия");
+                YAndE.Item2 = int.Parse(Console.ReadLine());
+
+                Simulator sim = new Simulator(YAndE.Item1, YAndE.Item2, VirusCollection.ChickenpoxVirus);
+                Observer observer = new Observer(ref sim);
+                observer.Start();
+                Results(sim);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        private static void Results(Simulator simulator)
+        {
+
+            /*
+                * Сколько человек было заражено;
+                * Сколько человек погибло;
+                * Сколько человек вылечилось;
+                * Сколько человек в популяции;
+                * Сколько дней длилась симуляция (необходимо, если симуляция была досрочно остановлена).
+            */
+
+
+            Console.WriteLine(simulator.Days == simulator.MaxDays ?
+                $"Заражено: {simulator.Illed}\n" +
+                $"Умершие: {simulator.Dead.Count}\n" +
+                $"Вылечилось: {simulator.Recovered}\n" +
+                $"Живые: {simulator.Alive.Count}"
+                :
+                $"Заражено: {simulator.Illed}\n" +
+                $"Умершие: {simulator.Dead.Count}\n" +
+                $"Вылечилось: {simulator.Recovered}\n" +
+                $"Живые: {simulator.Alive.Count}\n" +
+                $"Дней прошло: {simulator.Days}"
+                );
+        }
+    }
+
+    public static class VirusCollection
+    {
+        public static Virus ChickenpoxVirus => new Chickenpox("Chickenpox", false, 0.7f, 0.0001f);
+    }
+    class Chickenpox : Virus
+    {
+        private static Random _rand = new Random();
+
+        public Chickenpox(string Code, bool Reinfection, float InfectionCoef, float LethalityCoef) : base(Code, Reinfection, InfectionCoef, LethalityCoef)
+        {
+            _letality = LethalityCoef + (float)_rand.Next(-30, 30) / 100;
+        }
+        public override int AgeToInfect => 3;
+
+        public override int DayToRecover => 14;
+
+        public override bool Death(Person person)
+        {
+            if (_rand.NextDouble() <= Lethality)
+            {
+                person.Detach();
+                return true;
+            }
+            return false;
+        }
+
+        public override void Infect(Person person)
+        {
+            if (person.Immunity <= Infection)
+                person.Infect();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Utils/Simulator.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Utils
+{
+    public class Simulator
+    {
+        private const double _mortality = (double)14 / 1000;
+        private const double _birthrate = (double)8 / 1000;
+
+        private static Random _random = new Random();
+        private List<Person> _alive;
+        private List<Person> _dead;
+        private int _maxDays;
+        private int _day;
+        private Virus _virus;
+        private int _illed;
+        private int _recovered;
+
+        public int Days => _day;
+        public int MaxDays => _maxDays;
+        public int Illed => _illed; // больные
+        public int Recovered => _recovered;
+
+        public List<Person> Alive => _alive;
+        public List<Person> Dead => _dead;
+        public Simulator(int countPopulation, int maxDays, Virus virus)
+        {
+            _day = 1;
+            _maxDays = maxDays;
+            _alive = new List<Person>();
+            _dead = new List<Person>();
+            _virus = virus;
+            Population(countPopulation);
+        }
+        public void RunSimmulation()
+        {
+            StartInfection();
+            for (int i = 1; i <= _maxDays; i++)
+            {
+                _day = i;
+                _alive.RemoveAll((p) =>
+                {
+                    if (!p.IsAlive)
+                    {
+                        _dead.Add(p);
+                        return true;
+                    }
+                    return false;
+                });
+                if (i % 365 == 0)
+                {
+                    _alive.RemoveAll((p) =>
+                    {
+                        p.UpdateAge();
+                        if (p.Age >= p.MaxAge)
+                        {
+                            _dead.Add(p);
+                            return true;
+                        }
+                        return false;
+                    });
+                }
+                Infection();
+                Mortaliti();
+                Birth();
+            }
+        }
+        private void Mortaliti()
+        {
+            int range = (int)Math.Round(_alive.Count * _mortality / 365);
+            List<Person> a = _alive.GetRange(0, range);
+            _alive.RemoveRange(0, range);
+            _dead.AddRange(a);
+
+        }
+        private void Birth()
+        {
+            int range = (int)Math.Round(_alive.Count * _birthrate / 365);
+            for (int i = 0; i < range; i++)
+            {
+                Person person = new Person(
+                    _random.Next(0, 2) == 0 ? "Male" : "Famale", 0,
+                    (float)_random.Next(70, 76) / 100);
+                _alive.Add(person);
+            }
+        }
+        private void StartInfection()
+        {
+            for (int i = 0; i < Math.Round(_alive.Count * 0.02); i++)
+            {
+                _alive.Find((p) => (p.Age >= _virus.AgeToInfect) && (!p.Status)).Infect();
+                _illed++;
+            }
+            _alive = _alive.OrderBy(_ => _random.Next()).ToList();
+        }
+        private void Infection()
+        {
+            var allInfected = _alive.FindAll((p) => p.Status);
+            foreach (var p in allInfected)
+            {
+                if (_virus.Death(p)) continue;
+
+                if (p.UpdateInfection() == _virus.DayToRecover)
+                {
+                    if (!_virus.Reinfection)
+                    {
+                        p.CreateTotalImmunity();
+                    }
+                    p.Recover();
+                    _recovered++;
+                    continue;
+                }
+                if (_random.Next(101) >= 28)
+                {
+                    for (int i = 0; i < p.Friends / 2; i++)
+                    {
+                        Person meeting = _alive[_random.Next(0, _alive.Count)];
+                        if (!meeting.Status && meeting.Age >= _virus.AgeToInfect && !meeting.TotalImmunity)
+                        {
+                            _virus.Infect(meeting);
+                            _illed++;
+                        }
+                    }
+                }
+            }
+        }
+        private void Population(int countPopulation)
+        {
+            string[] gender = new string[2] { "Male", "Famale" };
+
+            for (int i = 0; i < countPopulation; i++)
+            {
+                Person person = new Person(
+                    gender[_random.Next(0, 2)],
+                    _random.Next(0, 81),
+                    (float)_random.Next(70, 76) / 100);
+                if (person.Age >= person.MaxAge)
+                    _dead.Add(person);
+                else
+                    _alive.Add(person);
+            }
+
+        }
+    }
+}
